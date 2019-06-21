08ac3b3ee8b6448651efe6a59d0a9b0e6afdaa1d
张珊珊 <zhangshanshan@qq.com>
2019-06-21 14:25:06 +0800

    完成博客预览

--- router/blogController/index.js
+++ router/blogController/index.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router')
 const blogController = require('./blogController')
 const queryBlogs = require('./queryBlogs')
+const queryBlogsById = require('./queryBlogsById')
 
 const router = new Router()
 
 router.use('/articles', blogController.routes())
 router.use('/articles', queryBlogs.routes())
+router.use('/articles', queryBlogsById.routes())
 
 module.exports = router
